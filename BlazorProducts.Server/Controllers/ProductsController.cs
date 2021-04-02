6b87a81e7afb1d4cd36a7b9d91b393a7d7919d89
warnov <warnov@gmail.com>
2021-04-02 14:16:14 -0500

    Server and Entities project integrated into the solution and source control

---
+++ BlazorProducts.Server/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using BlazorProducts.Server.Repository;
+using Entities.RequestFeatures;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace BlazorProducts.Server.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] ProductParameters productParameters)
+        {
+            var products = await _repo.GetProducts(productParameters);
+
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(products.MetaData));
+
+            return Ok(products);
+        }
+    }
+}
---
+++ BlazorProducts.Server/Repository/RepositoryExtensions/RepositoryProductExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using System.Linq;
+
+namespace BlazorProducts.Server.Repository.RepositoryExtensions
+{
+    public static class RepositoryProductExtensions
+    {
+        public static IQueryable<Product> Search(this IQueryable<Product> products, string searchTearm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTearm))
+                return products;
+            var lowerCaseSearchTerm = searchTearm.Trim().ToLower();
+            return products.Where(p => p.Name.ToLower().Contains(lowerCaseSearchTerm));
+        }
+    }
+}
